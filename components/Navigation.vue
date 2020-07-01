a78bfa480ca44520c1022e4b0d083da3259c1a61
wd30gsrc <gd28.c.tateno@gmail.com>
2020-07-01 22:27:34 +0900

    ナビとsassの追加

---
+++ components/Navigation.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <ul class="local-nav">
+      <li>
+        <nuxt-link to="/">
+          Top
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/about">
+          About
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/blog">
+          Blog
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/blog/99">
+          Blog99
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/element">
+          Element
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/axios">
+          Axios
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.local-nav {
+  list-style: none;
+  font-weight: bold;
+  a {
+    text-decoration: none;
+    color: #333;
+    line-height: 30px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,17 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-sample
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <navigation />
   </div>
 </template>
 
 <script>
-export default {}
+import Navigation from '~/components/Navigation.vue'
+
+export default {
+  components: {
+    Navigation
+  }
+}
 </script>
 
 <style>
